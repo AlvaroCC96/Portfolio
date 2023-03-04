b775f78daec1623e965c196454721564b04c7748
Álvaro Castillo Calabacero <alvarocastillocalabacero@gmail.com>
2023-03-04 00:52:29 -0300

    ADD: Header navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { MdNightsStay, MdWbSunny, } from "react-icons/md";
+
+
+const Header = ({ darkMode, setDarkMode }) => {
+  return (
+    <header className=" bg-slate-300 dark:bg-gray-900 text-gray-900 dark:text-teal-300">
+      <nav className="flex justify-between items-center p-5">
+        <h1 className="text-lg"> <span> Developer </span> </h1>
+        <div onClick={() => setDarkMode(!darkMode)}>
+          {darkMode ? (
+            <MdWbSunny className="text-2xl cursor-pointer" />
+          ) : (
+            <MdNightsStay className="text-2xl cursor-pointer" />
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
